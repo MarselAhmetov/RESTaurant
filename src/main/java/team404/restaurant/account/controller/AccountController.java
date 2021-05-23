84d700fb7dcee67acc4e083eb73949dfdbc3d468
v.buzanov <vladislav4869@gmail.com>
2021-05-23 20:47:52 +0300

    CORS configuration

--- src/main/java/team404/restaurant/account/controller/AccountController.java
+++ src/main/java/team404/restaurant/account/controller/AccountController.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import team404.restaurant.account.service.AccountService;
 @Tag(name = "Account", description = "Account controller")
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*", maxAge = 3600, allowedHeaders = "*")
 public class AccountController {
 
     private final AccountService accountService;
--- src/main/java/team404/restaurant/restaurateur/controller/RestaurateurController.java
+++ src/main/java/team404/restaurant/restaurateur/controller/RestaurateurController.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import team404.restaurant.restaurateur.service.RestaurateurService;
 @Tag(name = "Restaurateur")
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*", maxAge = 3600, allowedHeaders = "*")
 public class RestaurateurController {
 
     private final RestaurateurService restaurateurService;
